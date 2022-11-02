dfe1d8875f6cca219b6e6d96a7a8cd0fa3cfc8d3
Hudaibzaidi <117076704+Hudaibzaidi@users.noreply.github.com>
2022-11-02 13:22:43 +0500

    marksheet

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("my name is hudaib")
+
+var room = "abc"
+
+var bulb = 123
+
+var mobile = "kouch bhi"
+// legal variable
+
+var 345abc = "hudaib"
+// illegal variable
+
+var wifi = 6 + 6
+var network = 6 - 6
+var signals = 6 * 6
+var data = 6 / 6
+
+var acf = 12 + 56
+var acf = 12 + "56"
+var acf = "12" + 56
+// concatination
+
+// 1 to 8 chapter practice complete
+
